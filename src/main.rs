b9ffd8a9d1de74f0dcca84bd861a4e23c83b46b5
Immauel Albrecht <immanuel@perazim.ch>
2023-10-30 17:24:50 +0100

    Established connection to db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::Row;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "postgres://dbuser:mysecretpassword@localhost:5432/boardgamedb";
+    let pool = sqlx::postgres::PgPool::connect(url).await?;
+
+    let res = sqlx::query("SELECT 1 + 1 as sum").fetch_one(&pool).await?;
+
+    let sum: i32 = res.get("sum");
+    println!("1 + 1 = {sum}");
+
+    Ok(())
 }
