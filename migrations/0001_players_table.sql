07215b68ecd78ffd678d5422ad57132a74ada554
Immauel Albrecht <immanuel@perazim.ch>
2023-10-30 18:08:22 +0100

    Added build script

---
+++ migrations/0001_players_table.sql
@@ -0,0 +1,6 @@
+create table games (
+	title varchar not null,
+	designer varchar not null,
+	minplayers int not null,
+	maxplayers int not null
+);
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let url = "postgres://dbuser:mysecretpassword@localhost:5432/boardgamedb";
     let pool = sqlx::postgres::PgPool::connect(url).await?;
 
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
     let res = sqlx::query("SELECT 1 + 1 as sum").fetch_one(&pool).await?;
 
     let sum: i32 = res.get("sum");
